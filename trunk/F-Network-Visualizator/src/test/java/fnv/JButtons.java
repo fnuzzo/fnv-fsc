2881b6a80d22ef1f7917777d9d4ba5d0f9a6866a
giacomo.benvenuti <giacomo.benvenuti@adc575de-48e0-4274-9fe3-ea1071321f57>
2010-11-01 12:00:53 +0000

    Aggiustate dipendenze

---
+++ trunk/F-Network-Visualizator/src/test/java/fnv/JButtons.java
@@ -0,0 +1,37 @@
+package fnv;
+
+import java.awt.*;
+import javax.swing.*;
+
+/** Simple example illustrating the use of JButton, especially
+ *  the new constructors that permit you to add an image.
+ *  1998-99 Marty Hall, http://www.apl.jhu.edu/~hall/java/
+ */
+
+public class JButtons extends JFrame {
+  public static void main(String[] args) {
+    new JButtons();
+  }
+
+  public JButtons() {
+    super("Using JButton");
+
+    //WindowUtilities.setNativeLookAndFeel();
+    //addWindowListener(new ExitListener());
+    Container content = getContentPane();
+    content.setBackground(Color.white);
+    content.setLayout(new FlowLayout());
+    JButton button1 = new JButton("Java");
+    content.add(button1);
+    ImageIcon cup = new ImageIcon(getClass().getResource("/img/pause.png"));
+    JButton button2 = new JButton(cup);
+    content.add(button2);
+    JButton button3 = new JButton("Java", cup);
+    content.add(button3);
+    JButton button4 = new JButton("Java", cup);
+    button4.setHorizontalTextPosition(SwingConstants.LEFT);
+    content.add(button4);
+    pack();
+    setVisible(true);
+  }
+}

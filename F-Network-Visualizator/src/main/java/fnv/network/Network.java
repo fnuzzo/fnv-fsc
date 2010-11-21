4244cfa678c457c9b555acee43d40d2bcef64257
d.enrico <d.enrico@adc575de-48e0-4274-9fe3-ea1071321f57>
2010-11-21 12:12:44 +0000

    Il cubo dello spazio 3d viene disegnato in base alle massime coordinate dei nodi refactoring qua e la in Space Network ha solo il valore massimo tra tutte le coordinate dei nodi, non per ogni coordinata

--- F-Network-Visualizator/src/main/java/fnv/network/Network.java
+++ F-Network-Visualizator/src/main/java/fnv/network/Network.java
@@ -11,12 +11,14 @@ public class Network {
     private String name;
     /* l'elenco di nodi presenti nella rete */
     public NodesList nodesList;
-    /* valore massimo della coordinata x */
-    public final int maxX;
-    /* valore massimo della coordinata y */
-    public final int maxY;
-    /* valore massimo della coordinata z */
-    public final int maxZ;
+//    /* valore massimo della coordinata x */
+//    public final int maxX;
+//    /* valore massimo della coordinata y */
+//    public final int maxY;
+//    /* valore massimo della coordinata z */
+//    public final int maxZ;
+    /* valore massimo della coordinata in ogni direzione */
+    public final int maxCoordinate;
     /* le interazioni presenti nella rete */
     private InteractionsCube interactionCube;
 
@@ -24,7 +26,8 @@ public class Network {
 	name = "";
 	nodesList = new NodesList(new ArrayList<Node>());
 	interactionCube = new InteractionsCube();
-	maxX = maxY = maxZ = 0;
+//	maxX = maxY = maxZ = 0;
+	maxCoordinate = 0;
     }
 
     public Network(String name, NodesList nodesList, InteractionsCube interactionCube) {
@@ -32,23 +35,37 @@ public class Network {
 	this.nodesList = nodesList;
 	this.interactionCube = interactionCube;
 
-	int localMaxX = 0;
-	int localMaxY = 0;
-	int localMaxZ = 0;
+//	int localMaxX = 0;
+//	int localMaxY = 0;
+//	int localMaxZ = 0;
+//	for (Node node : nodesList.toArray()) {
+//	    if (node.x > localMaxX) {
+//		localMaxX = node.x;
+//	    }
+//	    if (node.y > localMaxY) {
+//		localMaxY = node.y;
+//	    }
+//	    if (node.z > localMaxZ) {
+//		localMaxZ = node.z;
+//	    }
+//	}
+//	this.maxX = localMaxX;
+//	this.maxY = localMaxY;
+//	this.maxZ = localMaxZ;
+
+	int localMaxCoordinate = 0;
 	for (Node node : nodesList.toArray()) {
-	    if (node.x > localMaxX) {
-		localMaxX = node.x;
+	    if (node.x > localMaxCoordinate) {
+		localMaxCoordinate = node.x;
 	    }
-	    if (node.y > localMaxY) {
-		localMaxY = node.y;
+	    if (node.y > localMaxCoordinate) {
+		localMaxCoordinate = node.y;
 	    }
-	    if (node.z > localMaxZ) {
-		localMaxZ = node.z;
+	    if (node.z > localMaxCoordinate) {
+		localMaxCoordinate = node.z;
 	    }
 	}
-	this.maxX = localMaxX;
-	this.maxY = localMaxY;
-	this.maxZ = localMaxZ;
+	maxCoordinate = localMaxCoordinate;
     }
 
     public String getName() {

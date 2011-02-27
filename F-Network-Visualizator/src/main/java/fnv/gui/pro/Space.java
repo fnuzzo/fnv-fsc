7355488af08623baad258a80e78affb177e5944c
giacomo.benvenuti <giacomo.benvenuti@adc575de-48e0-4274-9fe3-ea1071321f57>
2011-02-27 18:03:06 +0000

    Esperimenti texture

--- F-Network-Visualizator/src/main/java/fnv/gui/pro/Space.java
+++ F-Network-Visualizator/src/main/java/fnv/gui/pro/Space.java
@@ -79,6 +79,9 @@ public class Space extends PApplet {
     // http://mrfeinberg.com/peasycam/reference/index.html
     private PeasyCam cam;
 
+    //Immagine da usare come texture
+    PImage imageTexture;
+
     private int spaceWidth;
     private int spaceHeight;
     private double animationTimeSec = 2;
@@ -193,6 +196,10 @@ public class Space extends PApplet {
             f = loadFont("ArialMT-48.vlw");
             textFont(f, fontSize);
 
+            imageTexture = loadImage("networks/asia.jpg");
+            texmap = loadImage("networks/world32k.jpg");
+            initializeSphere(sDetail);
+
             smooth();
 
             frameRate(framerate);
@@ -210,6 +217,8 @@ public class Space extends PApplet {
             initializeTimer();
 
             cursor(MOVE);
+
+
 
     }
 
@@ -329,9 +338,9 @@ public class Space extends PApplet {
         }
     }
 
-    public void resizeSpace(Dimension d) {
+    /*public void resizeSpace(Dimension d) {
         resizeRenderer(d.width, d.height);
-    }
+    } */
 
     private void setAnimationTime() {
         animationTimer = new Timer((int) (animationTimeSec * 1000), new ActionListener() {
@@ -408,15 +417,15 @@ public class Space extends PApplet {
         } */
         noFill();
         beginShape();
-        texture(loadImage("networks/euro.jpg"));
+        texture(imageTexture);
         vertex(0, 0, 0,
                 0, 0);
         vertex(0, 0, space,
-                0, 200);
+                0, 450);
         vertex(space, 0, space,
-                200, 200);
+                450, 450);
         vertex(space, 0, 0,
-                200, 0);
+                450, 0);
         endShape(CLOSE);
         stroke(0, 0, 50);
     }
@@ -425,7 +434,10 @@ public class Space extends PApplet {
         pushMatrix();
         translate(space / 2, -space / 2, space / 2);
         noFill();
-        sphere(((network.maxCoordinate - 1) / 2) * box);
+        noStroke();
+        textureMode(IMAGE);
+        texturedSphere(space+180, texmap);
+        //sphere(((network.maxCoordinate - 1) / 2) * box);
 
         popMatrix();
     }
@@ -899,5 +911,137 @@ public class Space extends PApplet {
         }
 
     }
+
+    /**  http://processing.org/learning/library/texturedsphere.html
+ * Textured Sphere
+ * by Mike 'Flux' Chang (cleaned up by Aaron Koblin).
+ * Based on code by Toxi.
+ *
+ * A 3D textured sphere with simple rotation control.
+ * Note: Controls will be inverted when sphere is upside down.
+ * Use an "arc ball" to deal with this appropriately.
+ */
+
+    PImage bg;
+    PImage texmap;
+
+    int sDetail = 35;  // Sphere detail setting
+    float rotationX = 0;
+    float rotationY = 0;
+    float velocityX = 0;
+    float velocityY = 0;
+    float globeRadius = 450;
+    float pushBack = 0;
+
+    float[] cx, cz, sphereX, sphereY, sphereZ;
+    float sinLUT[];
+    float cosLUT[];
+    float SINCOS_PRECISION = 0.5f;
+    int SINCOS_LENGTH = PApplet.parseInt(360.0f / SINCOS_PRECISION);
+
+
+    public void initializeSphere(int res)
+{
+  sinLUT = new float[SINCOS_LENGTH];
+  cosLUT = new float[SINCOS_LENGTH];
+
+  for (int i = 0; i < SINCOS_LENGTH; i++) {
+    sinLUT[i] = (float) Math.sin(i * DEG_TO_RAD * SINCOS_PRECISION);
+    cosLUT[i] = (float) Math.cos(i * DEG_TO_RAD * SINCOS_PRECISION);
+  }
+
+  float delta = (float)SINCOS_LENGTH/res;
+  float[] cx = new float[res];
+  float[] cz = new float[res];
+
+  // Calc unit circle in XZ plane
+  for (int i = 0; i < res; i++) {
+    cx[i] = -cosLUT[(int) (i*delta) % SINCOS_LENGTH];
+    cz[i] = sinLUT[(int) (i*delta) % SINCOS_LENGTH];
+  }
+
+  // Computing vertexlist vertexlist starts at south pole
+  int vertCount = res * (res-1) + 2;
+  int currVert = 0;
+
+  // Re-init arrays to store vertices
+  sphereX = new float[vertCount];
+  sphereY = new float[vertCount];
+  sphereZ = new float[vertCount];
+  float angle_step = (SINCOS_LENGTH*0.5f)/res;
+  float angle = angle_step;
+
+  // Step along Y axis
+  for (int i = 1; i < res; i++) {
+    float curradius = sinLUT[(int) angle % SINCOS_LENGTH];
+    float currY = -cosLUT[(int) angle % SINCOS_LENGTH];
+    for (int j = 0; j < res; j++) {
+      sphereX[currVert] = cx[j] * curradius;
+      sphereY[currVert] = currY;
+      sphereZ[currVert++] = cz[j] * curradius;
+    }
+    angle += angle_step;
+  }
+  sDetail = res;
+}
+
+// Generic routine to draw textured sphere
+public void texturedSphere(float r, PImage t)
+{
+  int v1,v11,v2;
+  r = (r + 240 ) * 0.33f;
+  beginShape(TRIANGLE_STRIP);
+  texture(t);
+  float iu=(float)(t.width-1)/(sDetail);
+  float iv=(float)(t.height-1)/(sDetail);
+  float u=0,v=iv;
+  for (int i = 0; i < sDetail; i++) {
+    vertex(0, -r, 0,u,0);
+    vertex(sphereX[i]*r, sphereY[i]*r, sphereZ[i]*r, u, v);
+    u+=iu;
+  }
+  vertex(0, -r, 0,u,0);
+  vertex(sphereX[0]*r, sphereY[0]*r, sphereZ[0]*r, u, v);
+  endShape();
+
+  // Middle rings
+  int voff = 0;
+  for(int i = 2; i < sDetail; i++) {
+    v1=v11=voff;
+    voff += sDetail;
+    v2=voff;
+    u=0;
+    beginShape(TRIANGLE_STRIP);
+    texture(t);
+    for (int j = 0; j < sDetail; j++) {
+      vertex(sphereX[v1]*r, sphereY[v1]*r, sphereZ[v1++]*r, u, v);
+      vertex(sphereX[v2]*r, sphereY[v2]*r, sphereZ[v2++]*r, u, v+iv);
+      u+=iu;
+    }
+
+    // Close each ring
+    v1=v11;
+    v2=voff;
+    vertex(sphereX[v1]*r, sphereY[v1]*r, sphereZ[v1]*r, u, v);
+    vertex(sphereX[v2]*r, sphereY[v2]*r, sphereZ[v2]*r, u, v+iv);
+    endShape();
+    v+=iv;
+  }
+  u=0;
+
+  // Add the northern cap
+  beginShape(TRIANGLE_STRIP);
+  texture(t);
+  for (int i = 0; i < sDetail; i++) {
+    v2 = voff + i;
+    vertex(sphereX[v2]*r, sphereY[v2]*r, sphereZ[v2]*r, u, v);
+    vertex(0, r, 0,u,v+iv);
+    u+=iu;
+  }
+  vertex(sphereX[voff]*r, sphereY[voff]*r, sphereZ[voff]*r, u, v);
+  endShape();
+
+}
+
 
 }
